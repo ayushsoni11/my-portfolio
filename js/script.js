2bfd717e058ffb0aacbca0bb4e6dbe6a3c4dfaa4
ayushsoni11 <soniayush6441@gmail.com>
2024-11-29 10:24:38 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var a = 5;
+var b = 6;
+
+console.log(`The sum of ${a} and ${b} is ${a+b}`)
+
+let str = "HelloWorld"
+console.log(str.slice(-1));
+
+let arr = [1,2,3]
+console.log(arr.splice(-1));
+
+var obj = {
+    name : "Ayush",
+    age : 21, 
+    address : {  // Nested object
+        street : "123 Main St",
+        city : "Indore",
+    }
+}
+
+var d = new Date("25 Aug 2015");
+console.log(d)
+console.log(obj.address.city);
+
